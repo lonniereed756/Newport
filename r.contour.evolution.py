066fb0daa48b0ce7950e22200919daf400f66408
Vaclav Petras <wenzeslaus@gmail.com>
2014-07-16 13:39:35 -0400

    mask with negative buffer, direction from original aspect, speed as 1/slope

--- r.contour.evolution.py
+++ r.contour.evolution.py
@@ -4,7 +4,7 @@
 #
 # MODULE:       r.contour.evolution
 # AUTHOR(S):    Vaclav Petras
-# PURPOSE:      Outputs raster maps prepared for a Leaflet web map
+# PURPOSE:      Spatio-temporal contour vector field terrain analysis tool
 #
 # COPYRIGHT:    (C) 2014 by Vaclav Petras and the GRASS Development Team
 #
@@ -57,7 +57,9 @@ Created on Fri Feb 14 22:49:47 2014
 import os
 
 import grass.script.core as gcore
-from grass.script.core import run_command, parse_command
+from grass.script.core import parse_command, parse_key_val
+from grass.pygrass.modules import Module as run_command
+from grass.gunittest.gmodules import call_module
 from grass.script.raster import mapcalc as rmapcalc
 
 options, flags = gcore.parser()
@@ -89,8 +91,11 @@ for i, elevation in enumerate(elevations):
     contours_level_points = create_tmp_map_name(elevation + '_contours_level_' + level_str + '_points')
     contours_level_points_stc = create_tmp_map_name(elevation + '_contours_level_' + level_str + '_points_stc')
 
+    # now it wold be enough to just compute one level but we will
+    # eventually compute multiple levels anyway
     run_command('r.contour', input=elevation, output=contours, step=2)
     # extract one contour level (height)
+    # TODO: would it be possible to extract contours without attribute table?
     run_command('v.extract', input=contours, where='level=%f' % level,
                 output=contours_level)
     # convert contour line to (dense enough) set of points
@@ -106,10 +111,12 @@ stc_surface_increasing_points = create_tmp_map_name('stc_surface_increasing_poin
 stc_surface_increasing_elevation = create_tmp_map_name('stc_surface_increasing_elevation')
 stc_surface_increasing_slope = create_tmp_map_name('stc_surface_increasing_slope')
 stc_surface_increasing_aspect = create_tmp_map_name('stc_surface_increasing_aspect')
-stc_surface_decreasing_elevation = create_tmp_map_name('stc_surface_decreasing_elevation')
-stc_surface_decreasing_slope = create_tmp_map_name('stc_surface_decreasing_slope')
-stc_surface_decreasing_aspect = create_tmp_map_name('stc_surface_decreasing_aspect')
-stc_surface_decreasing_slope_inverted = create_tmp_map_name('stc_surface_decreasing_slope_inverted')
+#stc_surface_decreasing_elevation = create_tmp_map_name('stc_surface_decreasing_elevation')
+#stc_surface_decreasing_slope = create_tmp_map_name('stc_surface_decreasing_slope')
+#stc_surface_decreasing_aspect = create_tmp_map_name('stc_surface_decreasing_aspect')
+#stc_surface_decreasing_slope_inverted = create_tmp_map_name('stc_surface_decreasing_slope_inverted')
+speed = create_tmp_map_name('speed')
+direction = create_tmp_map_name('direction')
 
 # patch points of the same original height (contour level) from all years together
 run_command('v.patch', input=contours_level_points_stcs,
@@ -121,23 +128,61 @@ run_command('v.surf.rst', input=stc_surface_increasing_points,
             aspect=stc_surface_increasing_aspect)
 
 # invert surface to have time going down the hill
-rmapcalc('%s = -%s' % (stc_surface_decreasing_elevation, stc_surface_increasing_elevation))
+#rmapcalc('%s = -%s' % (stc_surface_decreasing_elevation, stc_surface_increasing_elevation))
 # compute surface properties
 # computing slope (surface gradient magnitude)
 # and aspect (surface gradient direction projected to xy plane)
-run_command('r.slope.aspect', elevation=stc_surface_decreasing_elevation,
-            slope=stc_surface_decreasing_slope,
-            aspect=stc_surface_decreasing_aspect,
-            format='degrees', precision='FCELL',
-            zfactor=1.0, min_slp_allowed=0.0)
+#run_command('r.slope.aspect', elevation=stc_surface_increasing_elevation,
+#            slope=stc_surface_decreasing_slope,
+#            aspect=stc_surface_decreasing_aspect,
+#            format='degrees', precision='FCELL',
+#            zfactor=1.0, min_slp_allowed=0.0)
 # invert slope to have higher values where slope is lower
 # to avoid negative values subtract from global max instead of 0
-# TODO: some other "inversion" might be better
-stc_surface_stats = parse_command('r.univar', map=stc_surface_decreasing_slope,
-                                  flags='g')
-rmapcalc('%s = %s - %s' % (stc_surface_decreasing_slope_inverted,
-                           stc_surface_stats['max'],
-                           stc_surface_decreasing_slope))
+# using 90 as maximum slope possible
+rmapcalc('{speed} = 1. / {slope}'.format(speed=speed,
+                              slope=stc_surface_increasing_slope))
+
+rmapcalc('eval(f = 180 + {a})\n{d} = if(f > 360, f - 360, f)'.format(d=direction, a=stc_surface_increasing_aspect))
+
+num_elev = len(elevations)
+expr_eval_values = []
+expr_comp_values = []
+for i in range(num_elev):
+    if num_elev > i + 1:
+        expr_comp = ""
+        a = elevations[i]
+        b = elevations[i + 1]
+        r = 'rc' + a + b
+        expr_comp += "{r} = xor({a} > {l}, {b} > {l})".format(a=a, b=b, l=level, r=r)
+        expr_comp_values.append(expr_comp)
+        expr_eval_values.append("{r}".format(r=r))
+
+# TODO: apply mask at the beginning/before interpolation
+mask = create_tmp_map_name('mask')
+buffered_mask_intermediate = create_tmp_map_name('buffered_mask_intermediate')
+buffered_mask = create_tmp_map_name('buffered_mask')
+
+# r.mapcalc xor() takes only two arguments
+multixor = 'if({a}, 0, {o})'.format(a=' && '.join(expr_eval_values), o=' || '.join(expr_eval_values))
+expr_mask = "eval(" + ", ".join(expr_comp_values) + ")\n{m} = {mx}".format(m=mask, mx=multixor)
+
+print expr_mask
+rmapcalc(expr_mask)
+run_command('r.null', map=mask, setnull=0)
+
+# region = parse_key_val(call_module('g.region', flags='g'))
+
+# distance = 1.5 * (float(region['nsres']) + float(region['ewres'])) / 2.
+
+# here it would be much better to have distance in number of cells
+# manual and default value adds 0.01 at the end
+# works with C version of r.grow
+# this will remove small null areas (5) and then get to the old state and apply buffer (3)
+run_command('r.grow', input=mask, output=buffered_mask_intermediate, radius=5.01)
+run_command('r.grow', input=buffered_mask_intermediate, output=buffered_mask, radius=-7.01)
+
+
 
 # in GUI, add a raster flow layer:
 #  d.rast.arrow map=aspect magnitude_map=slope \
